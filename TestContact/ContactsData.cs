c555d93aaca94bcec4f8a10abe5a768c03e3389e
Irina <Irina@LAPTOP-DADEHNOD>
2018-08-07 20:01:37 +0300

    Test Contacts with other data

---
+++ TestContact/ContactsData.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Addressbook
+{
+    class ContactsData
+    {
+        private string firstname;
+        private string lastname;
+        private string middlename ="";
+        private string nickname = "";
+        private string title = "";
+        private string company = "";
+        private string address = "";
+        private string home = "";
+        private string mobile = "";
+        private string fax = "";
+        private string work = "";
+        private string email = "";
+        private string email2 = "";
+
+
+
+
+        public ContactsData(string firstname, string lastname)
+        {
+            this.firstname = firstname;
+            this.lastname = lastname;
+        }
+
+        public ContactsData(string firstname, string lastname, string middlename, string nickname,
+            string title, string company, string address, string home, string mobile, string fax, string work, string email,
+            string email2)
+        {
+            this.firstname = firstname;
+            this.lastname = lastname;
+            this.middlename = middlename;
+            this.nickname = nickname;
+            this.title = title;
+            this.company = company;
+            this.address = address;
+            this.home = home;
+            this.mobile = mobile;
+            this.fax = fax;
+            this.work = work;
+            this.email = email;
+            this.email2 = email2;  
+        }
+
+        public string Firstname
+        {
+            get
+            {
+                return firstname;
+            }
+            set
+            {
+                firstname = value;
+            }
+        }
+        public string Lastname
+        {
+            get
+            {
+                return lastname;
+            }
+            set
+            {
+                lastname = value;
+            }
+        }
+    }
+}
